3172bdc47d38aa7607a6023f2c8812f0dc21d53e
Ben <benblott@gmail.com>
2023-08-27 19:41:32 -0500

    Finished forecasts.R for singlne name

--- forecasts.R
+++ forecasts.R
@@ -19,7 +19,7 @@ all_data <- map_dfr(dir('./data'), function(x){
 
 ### ...a single name:
 
-# get one name
+# get one name: "Delilah"
 df_D <- all_data %>%
     filter(name == "Delilah")
 
@@ -29,83 +29,38 @@ df_D <- df_D |>
 
 # get summary statistics
 summary(df_D)
-summary(df_D$count)
+# summary(df_D$count)
 
 # convert to time_series
-ts(df_D, start=c(1880))
 df_D_ts <- ts(df_D,start=c(1880))
 
 # Check the plot
-plot.ts(ts(df_D,start=c(1880)))
 plot.ts(df_D_ts)
 
-### Set up training and test sets
-# Baseline model
-
-# Find the right parameters for HoltWinter (no seasonal, but a trend)
+# Forecast HoltWinter model (no seasonal, but a trend)
+HoltWinters(df_D_ts, beta = TRUE, gamma = FALSE)
 D_HW_forecasts <- HoltWinters(df_D_ts, beta = TRUE, gamma = FALSE)
 
-HoltWinters(df_D_ts, beta = TRUE, gamma = FALSE)
-# D_HW_forecasts <- HoltWinters(df_D_ts, beta = FALSE, gamma = FALSE)
+# check values
 D_HW_forecasts$fitted
 
 # plot ts vs forecasts
 plot(D_HW_forecasts)
 
-# Run Holt Winters
+# Run Holt Winters for years beyond data set
+# 1
+forecast:::forecast.HoltWinters(D_HW_forecasts, h=1)
+
+# 10
 forecast:::forecast.HoltWinters(D_HW_forecasts, h=10)
 
-# select count column and last year for y-variable
-y <- df_D_ts["2022-01-01","count"]
-
-# select all but last year for X-set
-X <- window(df_D_ts,
-            end = "2021-01-01")
-# select only count column
-X <- X[,"count"]
-
-### predict
-
-# visualize
-plot.ts(X)
-
-# naive(X, h = 12)
-
-# 10 year model
+# 100
+forecast:::forecast.HoltWinters(D_HW_forecasts, h=100)
 
 # create a df with all years
 # get the name and bind with year
 # check for missing values
 # impute missing values
-
-
-
-# get just year and count vars
-df_D_ts <- df_D %>%
-    select(year, count)
-
-# data visual
-plot(df_D_ts)
-# plot trend line
-abline(reg = lm(df_D_ts$count ~ df_D_ts$year))
-
-# convert to time series
-df_D_ts <- ts(df_D_ts)
-
-# ts_D_ts <- ts(df_D_ts$count)
-
-# decompose data
-# ddf <- decompose(df_D_ts, "multiplicative")  # there is no seasonality, so this doesn't apply here
-
-# create arima model
-mymodel <- auto.arima(df_D_ts[,1])
-
-# plot the residuals
-plot.ts(mymodel$residuals)
-
-# forecast for the next ten years
-myforecast <- forecast(mymodel, h=10)
-plot(myforecast)
 
 ##### Forecast a few names
 
