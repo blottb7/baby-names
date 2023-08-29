dcf84c2ecdd2fbe81404c50c1fec5e8f4505410e
Ben <benblott@gmail.com>
2023-08-29 09:34:11 -0500

    names_with_missing_values.R: solved empty df issue

--- names_with_missing_values.R
+++ names_with_missing_values.R
@@ -6,11 +6,34 @@ ts(0, start = 1880, end = 2022, frequency = 1)
 count_matrix <- matrix(0, nrow = length(unique(all_data$year)))
 # seq(1880, 2022, 1)
 year_matrix <- matrix(seq(1880, 2022, 1))
-matrix(c(0, seq(2023, 2032, 1)), nrow = length(unique(all_data$year)), ncol = 2)
+# matrix(c(0, seq(2023, 2032, 1)), nrow = length(unique(all_data$year)), ncol = 2)
+
+# join the two matrices
+df_empty <- as.data.frame(count_matrix %>%
+    bind_cols(year_matrix))
+names(df_empty) <- c("count", "year")
+
+df_year <- as.data.frame(year_matrix)
+names(df_year) <- "year"
+
+df_year %>%
+    left_join(df_D)
 
 # get a name with missing counts
 df_D <- all_data %>%
     filter(name == "Zylon")
+
+# fill in values where they exist
+for(year in df_empty$year) {
+    
+    # if(year %in% df_D$year) {
+    #     print(year)
+    # }
+    
+    ifelse(year %in% df_D_copy, print(df_D_copy$count), 0)
+    
+}
+
 
 # reserve that data
 df_D_copy <- df_D
