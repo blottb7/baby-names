de711dc2d97fc60e1c6fe6d97d8296b4de0ee196
Ben <benblott@gmail.com>
2023-08-29 09:34:58 -0500

    Removed #'d code

--- names_with_missing_values.R
+++ names_with_missing_values.R
@@ -1,12 +1,7 @@
-
-
 # create a df with all years
 ts(0, start = 1880, end = 2022, frequency = 1)
-# as.data.frame(matrix(0, nrow = length(unique(all_data$year))))
 count_matrix <- matrix(0, nrow = length(unique(all_data$year)))
-# seq(1880, 2022, 1)
 year_matrix <- matrix(seq(1880, 2022, 1))
-# matrix(c(0, seq(2023, 2032, 1)), nrow = length(unique(all_data$year)), ncol = 2)
 
 # join the two matrices
 df_empty <- as.data.frame(count_matrix %>%
